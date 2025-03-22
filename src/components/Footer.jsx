c2bde6fa7f8cdc0fae98a3e75ab2b18dd4ab88e4
Onurcan Yelestepe <oyelestepe1@gmail.com>
2025-03-22 19:28:52 +0300

    social media icons added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>&copy; 2025 Invoice Generator. Tüm hakları saklıdır.</p>
+      <a href='#'><FaInstagram className='footer-icon'/></a>
+      <a href='#'><FaXTwitter className='footer-icon'/></a>
+      <a href='#'><FaLinkedin className='footer-icon'/></a>
+      <a href='#'><FaYoutube className='footer-icon'/></a>
+      <p>&copy; 2025 Invoice Generator. All rights reserved.</p>
     </div>
   );
 };
