aece02bc5fbf3c4b315af4804ae7c2e0e582408c
Onurcan Yelestepe <oyelestepe1@gmail.com>
2025-03-25 19:28:01 +0300

    hamburger menu added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { auth } from '../firebaseConfig';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { FaBars, FaTimes } from 'react-icons/fa'; // Import icons for the hamburger menu
 
 const Navbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to manage menu visibility
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -19,28 +21,35 @@ const Navbar = () => {
       await signOut(auth);
       navigate('/');
     } catch (error) {
-      console.error('Çıkış yaparken hata:', error);
+      console.error('Error logging out:', error);
     }
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Toggle the menu visibility
   };
 
   return (
     <nav className="navbar">
       <Link to="/">
         <img src="/logo.png" alt="Logo" className="navbar-logo" />
-      </Link>      
-      <ul className="navbar-links" id="primary">
-        <li><Link to="/">Anasayfa</Link></li>
-        <li><Link to="/faq">FaQ</Link></li>        
+      </Link>
+      <button className="hamburger-menu" onClick={toggleMenu}>
+        {isMenuOpen ? <FaTimes /> : <FaBars />}
+      </button>
+      <ul className={`navbar-links ${isMenuOpen ? 'active' : ''}`} id="primary">
+        <li><Link to="/">Homepage</Link></li>
+        <li><Link to="/faq">FAQ</Link></li>
         {isLoggedIn ? (
           <>
             <li><Link to="/dashboard">Dashboard</Link></li>
-            <li><Link to="/create-invoice">Fatura Oluştur</Link></li>
-            <li><button onClick={handleLogout} className="logout-button">Çıkış Yap</button></li>
+            <li><Link to="/create-invoice">Create Invoice</Link></li>
+            <li><button onClick={handleLogout} className="logout-button">Logout</button></li>
           </>
         ) : (
           <>
-            <li><Link to="/login">Giriş Yap</Link></li>
-            <li><Link to="/register">Kayıt Ol</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
           </>
         )}
       </ul>
