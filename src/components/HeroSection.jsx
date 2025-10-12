1c9955176eaf3a91aee29c6f42fa282967579d07
Onurcan Yelestepe <oyelestepe1@gmail.com>
2025-10-12 21:33:20 +0300

    refactor HeroSection component and update styles for improved layout and aesthetics

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,32 +1,12 @@
-import React, { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
+import React from 'react';
 
 const HeroSection = () => {
-  const heroTextRef = useRef(null);
-  const heroButtonRef = useRef(null);
-
-  useEffect(() => {
-    gsap.from(heroTextRef.current, {
-      opacity: 0,
-      y: 50,
-      duration: 1,
-      ease: 'power2.out',
-    });
-
-    gsap.from(heroButtonRef.current, {
-      opacity: 0,
-      scale: 0.8,
-      duration: 1,
-      delay: 0.5,
-      ease: 'power2.out',
-    });
-  }, []);
 
   return (
     <div className="hero-container">
-      <h1 ref={heroTextRef}>Generate & Send Professional Invoices Instantly!</h1>
+      <h1><span className='hero-span'>Generate & Send Professional</span> Invoices Instantly!</h1>
       <p>Tired of complicated invoicing? Generate professional invoices effortlessly, download them as PDF, or send them directly via email all in just a few clicks.</p>
-      <div className="hero-buttons" ref={heroButtonRef}>
+      <div className="hero-buttons">
         <button className="cta-button">Generate an Invoice Now</button>
       </div>
     </div>
